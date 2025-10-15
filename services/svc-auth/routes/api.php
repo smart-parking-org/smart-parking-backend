25ac81b710b20d68fab937bc3dd3fb04f0d179e0
Trần Hoàng Kha <hoangkhadev@gmail.com>
2025-10-15 13:28:10 +0700

    fix(auth): update api route for refresh token

--- services/svc-auth/routes/api.php
+++ services/svc-auth/routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('refresh', [AuthController::class, 'refreshToken']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
 
     Route::middleware(['auth:api'])->group(function () {
         Route::get('me', [AuthController::class, 'me']);
