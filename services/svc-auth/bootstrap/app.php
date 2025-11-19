12d83ded3668297877313b23415c37f3f98e2da8
hoangkhadev <hoangkhadev@gmail.com>
2025-11-19 22:56:50 +0700

    fix: fix middleware and vehicle resubmit

--- services/svc-auth/bootstrap/app.php
+++ services/svc-auth/bootstrap/app.php
@@ -15,8 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'ensure.admin' => EnsureAdmin::class,
-        ]);
-        $middleware->alias([
             'ensure.staff' => \App\Http\Middleware\EnsureStaff::class,
         ]);
     })
